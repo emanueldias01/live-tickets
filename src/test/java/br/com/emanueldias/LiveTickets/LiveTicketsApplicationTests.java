fe79afc396e6736c916ec5aaa0539d88539c899a
Emanuel <emanuel@MacBook-Air-de-Emanuel.local>
2025-12-30 18:07:21 -0300

    update files

---
+++ src/test/java/br/com/emanueldias/LiveTickets/LiveTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias.LiveTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
