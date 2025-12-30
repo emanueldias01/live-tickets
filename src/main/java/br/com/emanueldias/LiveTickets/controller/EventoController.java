018b8f3aafde77a5fabf33bb6e90229a6fcf0437
emanueldias01 <emanueldias01dev@gmail.com>
2025-12-30 18:54:29 -0300

    get by id

--- src/main/java/br/com/emanueldias/LiveTickets/controller/EventoController.java
+++ src/main/java/br/com/emanueldias/LiveTickets/controller/EventoController.java
@@ -5,9 +5,11 @@ import br.com.emanueldias.LiveTickets.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.awt.*;
 
@@ -21,5 +23,10 @@ public class EventoController {
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoResponseDTO> getAllEventos() {
         return eventoService.getAllEventos();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<EventoResponseDTO> getEventoById(@PathVariable Long id) {
+        return eventoService.getEventoById(id);
     }
 }
--- src/main/java/br/com/emanueldias/LiveTickets/service/EventoService.java
+++ src/main/java/br/com/emanueldias/LiveTickets/service/EventoService.java
@@ -3,8 +3,11 @@ package br.com.emanueldias.LiveTickets.service;
 import br.com.emanueldias.LiveTickets.dto.EventoResponseDTO;
 import br.com.emanueldias.LiveTickets.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -14,6 +17,12 @@ public class EventoService {
 
     public Flux<EventoResponseDTO> getAllEventos() {
         return eventoRepository.findAll()
+                .map(EventoResponseDTO::toDto);
+    }
+
+    public Mono<EventoResponseDTO> getEventoById(Long id) {
+        return eventoRepository.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .map(EventoResponseDTO::toDto);
     }
 }
